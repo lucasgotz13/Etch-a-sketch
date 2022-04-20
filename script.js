5d39dcdf37859dfea5b642c6847ef77aa2ce4bf4
Lucas Gotz <tutigotz@gmail.com>
2022-04-20 10:04:34 -0300

    Added reset button

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 const container = document.querySelector(".container")
-const boxes = document.querySelector(".boxes")
+const boxes = document.querySelectorAll(".boxes")
 const resetButton = document.querySelector("#reset")
 
 function createGrid(gridSize) {
-    for (i = 0; i < gridSize; i++) {
-        const div = document.createElement("div");
-        div.classList.add("row");
-        container.appendChild(div);
-        for (j = 0; j < gridSize; j++) {
-            const boxDiv = document.createElement("div");
-            boxDiv.classList.add("boxes");
-            div.appendChild(boxDiv)
-        }
+  for (i = 0; i < gridSize; i++) {
+    const div = document.createElement("div");
+    div.classList.add("row");
+    container.appendChild(div);
+    for (j = 0; j < gridSize; j++) {
+      const boxDiv = document.createElement("div");
+      boxDiv.classList.add("boxes");
+      div.appendChild(boxDiv)
     }
+  }
 }
 
 
 
 container.addEventListener('mouseover', e => {
-    e.target.classList.add("boxes-painted")
+  e.target.classList.add("boxes-painted")
 })
 
 resetButton.addEventListener('click', () => {
-    boxes.classList.remove("boxes-painted")
+  document
+    .querySelectorAll(".boxes")
+    .forEach((e) => e.classList.remove("boxes-painted"));
 })
-createGrid(20)
+
+let gridSize = parseInt(prompt("How big do you want the grid to be?"))
+createGrid(gridSize)
